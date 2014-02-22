aaecd8938b208ba62b6928722f702ed31d78ac1f
Casper Oakley <casper_oakley@hotmail.com>
2014-02-22 14:32:49 +0000

    Added base yo

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('assassinApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    $http.get('/api/awesomeThings').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+  });
