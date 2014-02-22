347a7cd5faa35d41a86b3faf8a5a3de5228fe454
Casper Oakley <casper_oakley@hotmail.com>
2014-02-22 17:53:22 +0000

    PROGRESS

--- app.js
+++ app.js
@@ -4,11 +4,11 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , input = require('./routes/inputMess')
   , http = require('http')
-  , path = require('path');
+  , hbs = require('hbs');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
 
 var twilio = require('twilio');
 
@@ -16,40 +16,23 @@ var client = new twilio.RestClient('ACd33440265fbfd13384a294c4aca2f63b','0495474
 
 var sys = require('sys');
 
-/*client.sms.messages.create({
-	to:'+447810494417',
-	from:'+441482240221',
-	body:'Test'
-}, function(error,message){
-	if(!error){
-		console.log('wahey message sent successfully.');
-		console.log('ID is ' + message.sid);
-	} else{
-		console.log('error: ' + error);
-	}
-});*/
 
 var app = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static('public'));
+app.set('view engine','html');
+app.engine('html',hbs.__express);
+app.use(express.bodyParser());
+
+app.get('/', function(req,res){
+  res.render('index',{title:"stabman"});
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
+app.get('/leaderboard', function(req,res){
+	res.render('leaderboard',{title:"LeaderBoard"})
 });
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-app.get('/input', input.inputMess);
 
 app.post('/incoming', function(req, res) {
   var message = req.body.Body;
@@ -57,8 +40,42 @@ app.post('/incoming', function(req, res) {
   sys.log('From: ' + from + ', Message: ' + message);
   var twiml = '<?xml version="1.0" encoding="UTF-8" ?>n<Response>n<Sms>Thanks for your text, well be in touch.</Sms>n</Response>';
   res.send(twiml, {'Content-Type':'text/xml'}, 200);
+  client.sms.messages.create({
+	  //to:'+447884036188',
+	  to: from,
+	  from:'+441482240221',
+	  //body:'Test'
+	  body: message
+  }, function(error,message){
+	  if(!error){
+		  console.log('wahey message sent successfully.');
+		  console.log('ID is ' + message.sid);
+	  } else{
+		  console.log('error: ' + error);
+	  }
+  });
 });
 
+
+
+var db = mongoose.connection;
+db.on('error',console.error.bind(console, 'connection error:'));
+db.once('open', function callback(){
+});
+
+var user = mongoose.Schema({
+	name: String,
+	ID: Number,
+	isPicked: Boolean
+});
+
+var user1 = mongoose.model('account',user);
+
+var testUser1 = new user1({ name: 'Casper Oakley', ID: 1,isPicked: false});
+var testUser2 = new user1({ name: 'Sam Berkay', ID: 2,isPicked: false});
+
+console.log(testUser1.name);
+console.log(testUser2.name);
 
 
 http.createServer(app).listen(app.get('port'), function(){
